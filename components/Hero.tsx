3f46dee054d2a4f87a6abfb5fbf38c7719c405ba
Priyanshu Chaurasia <priyanshuchaurasia8840@gmail.com>
2025-07-14 16:53:25 +0530

    Refactor code structure for improved readability and maintainability

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,17 +1,20 @@
 "use client";
 import Image from "next/image";
-import CustomButton from "./CustomButton";
+import  CustomButton  from "./CustomButton";
 
 const Hero = () => {
-  const handleScroll = () => {};
+  const handleScroll = () => {
+    
+  };
+
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
-        <h1 className="hero__title ">
-          Find, book, or rent a car - quickly and easily!
+        <h1 className="hero__title">
+          Find, book, rent a car—quick and super easy!
         </h1>
 
-        <p className=".hero__subtitle">
+        <p className="hero__subtitle">
           Streamline your car rental experience with our effortless booking
           process.
         </p>
@@ -24,8 +27,10 @@ const Hero = () => {
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
-          <Image src= ""/>
+          <Image src="/4.png" alt="hero" fill className="object-contain" />
         </div>
+
+        <div className="hero__image-overlay" />
       </div>
     </div>
   );
