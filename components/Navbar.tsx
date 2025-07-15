a38f30307191d471f22a0a1d6618004a0f8e0b6f
Priyanshu Chaurasia <priyanshuchaurasia8840@gmail.com>
2025-07-15 12:50:48 +0530

    Add Preview.png image file to the public directory

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,22 @@
 import Image from "next/image";
+import Link from "next/link";
+import CustomButton from "./CustomButton";
 
 const Navbar = () => {
   return (
-    <div>
+    <header className="w-full absolute z-10">
+        <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4">
+            <Link href="/" className="flex justify-center items-center">
+            <Image src="/5.png" alt="Car Hub Logo" width={118} height={18} className="object-contain" />
+            </Link>
+            <CustomButton
+            title ="Sign In"
+            btnType="button"
+            containerStyles="text-primary-blue rounded-full bg-white min-w-[130px]"
+            />
+        </nav>
       
-    </div>
+    </header>
   )
 }
 
