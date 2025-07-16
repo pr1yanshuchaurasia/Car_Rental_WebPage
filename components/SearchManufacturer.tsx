cff45f283299265276fb344b105ba51f3e98fd14
Priyanshu Chaurasia <priyanshuchaurasia8840@gmail.com>
2025-07-16 16:01:37 +0530

    feat: enhance SearchManufacturer component with Combobox for improved manufacturer selection

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,9 +1,25 @@
+"use client";
 import { SearchManufacturerProps } from "@/types";
-import image from "next/image";
+import { Combobox, Transition } from "@headlessui/react";
+import Image from "next/image";
 
 const SearchManufacturer = ({manufacturer,setManufacturer}:SearchManufacturerProps) => {
   return (
-    <div>
+    <div className="search-manufacturer">
+        <Combobox>
+            <div className="relative w-full">
+                <Combobox.Button className="absolute top-[14px]">
+                    <Image
+                    src="/car-logo.svg"
+                    alt="car logo"
+                    width={20}
+                    height={20}
+                    className="ml-4"
+                    />
+                </Combobox.Button>
+
+            </div>
+        </Combobox>
       
     </div>
   )
