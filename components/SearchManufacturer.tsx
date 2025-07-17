6014473d3b4d8d4cbe9a1a68c7032886ff500a91
Priyanshu Chaurasia <priyanshuchaurasia8840@gmail.com>
2025-07-17 16:20:59 +0530

    feat: implement search functionality in SearchManufacturer component

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -2,8 +2,17 @@
 import { SearchManufacturerProps } from "@/types";
 import { Combobox, Transition } from "@headlessui/react";
 import Image from "next/image";
+import { useState, Fragment } from "react";
+import { manufacturers } from "@/constants";
 
 const SearchManufacturer = ({manufacturer,setManufacturer}:SearchManufacturerProps) => {
+  const [query, setQuery] = useState("");
+  const filteredManufacturers = query === "" 
+    ? manufacturers 
+    : manufacturers.filter((item) => 
+        item.toLowerCase().replace(/\s+/g, "")
+        .includes(query.toLowerCase().replace(/\s+/g, ""))
+      );
   return (
     <div className="search-manufacturer">
         <Combobox>
@@ -20,7 +29,18 @@ const SearchManufacturer = ({manufacturer,setManufacturer}:SearchManufacturerPro
                 <Combobox.Input className="search-manufacturer__input"
                 placeholder="Volkswagen"
                 displayValue={(manufacturer: string) => manufacturer}
+                onChange={(e)=> setQuery(e.target.value)}
                 />
+                <Transition
+                as ={Fragment}
+                leave="transition ease-in duration-100"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+                afterLeave={() => setQuery("")}
+                >
+                  <Combobox.Options></Combobox.Options>
+
+                </Transition>
 
             </div>
         </Combobox>
