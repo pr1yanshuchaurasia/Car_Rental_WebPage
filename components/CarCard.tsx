fe86f5e5d8b79a4c7d90d4d25303cca5728ceae7
Priyanshu Chaurasia <priyanshuchaurasia8840@gmail.com>
2025-07-21 18:06:58 +0530

    feat: add image display to CarCard component

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -40,6 +40,9 @@ const CarCard = ({ car }: CarCardProps) => {
             /day
         </span>
       </p>
+      <div className="relative w-full h-40 my-3 object-contain">
+        <Image src="/hero.png" alt="car model" fill priority className="object-contain"/>
+      </div>
     </div>
   );
 };
