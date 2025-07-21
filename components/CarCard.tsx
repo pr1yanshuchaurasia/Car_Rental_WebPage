18034e693ce7560179d4b61108c7db53dd927cd4
Priyanshu Chaurasia <priyanshuchaurasia8840@gmail.com>
2025-07-21 18:24:21 +0530

    feat: add transmission display to CarCard component

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -43,6 +43,16 @@ const CarCard = ({ car }: CarCardProps) => {
       <div className="relative w-full h-40 my-3 object-contain">
         <Image src="/hero.png" alt="car model" fill priority className="object-contain"/>
       </div>
+      <div className="relative flex w-full mt-2">
+        <div className="flex group-hover:invisibl w-full justify-between text-gray">
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image src= "/steering-wheel.svg" width={20} height={20} alt="steering Wheel"/>
+            <p className="text-[14 px]">
+              {transmission === "a" ? "Automatic" : "Manual"}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
