99b315beb0cacf01882ee56a62a8fcd18b8635f2
Priyanshu Chaurasia <priyanshuchaurasia8840@gmail.com>
2025-07-23 17:42:26 +0530

    feat: add CarDetails component and integrate it into CarCard

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { useState } from "react";
 import { CustomButton } from "@/components";
 import { calculateCarRent } from "@/utils";
+import CarDetails from "./CarDetails";
 
 interface CarCardProps {
   car: CarProps;
@@ -69,6 +70,8 @@ const CarCard = ({ car }: CarCardProps) => {
           <CustomButton title="View More" containerStyles="w-full py-[16px] rounded-full bg-primary-blue" textStyles="text-white text-[14px] leading-[17px] font-bold" rightIcon="/right-arrow.svg" handleClick={()=> setIsOpen(true)} />
         </div>
       </div>
+
+      <CarDetails/>
     </div>
   );
 };
---
+++ components/CarDetails.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+const CarDetails = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CarDetails
