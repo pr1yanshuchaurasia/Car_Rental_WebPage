bc891c93026f24f29723f1622083877f63cb65a5
Priyanshu Chaurasia <priyanshuchaurasia8840@gmail.com>
2025-07-29 21:02:05 +0530

    fix: correct import statement and enhance SearchButton component with image

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,10 +1,18 @@
 "use client";
-import image from "next/image";
+import Image from "next/image";
 import { SearchManufacturer } from "@/components";
 import { useState } from "react";
 
-const SearchButton = ({ otherClasses }) => (
-  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}></button>
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image
+      src="/magnifying-glass.svg"
+      alt="magnifying glass"
+      width={40}
+      height={40}
+      className="object-contain"
+    />
+  </button>
 );
 
 const SearchBar = () => {
