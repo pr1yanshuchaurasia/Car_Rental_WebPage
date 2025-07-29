203b1995232e2116545c51e84e26b81ae3041fd8
Priyanshu Chaurasia <priyanshuchaurasia8840@gmail.com>
2025-07-29 20:59:14 +0530

    fix: update fetchCars function to use correct model parameter for API request

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,25 +1,26 @@
 "use client";
 import image from "next/image";
 import { SearchManufacturer } from "@/components";
-import {useState} from "react";
+import { useState } from "react";
 
+const SearchButton = ({ otherClasses }) => (
+  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}></button>
+);
 
 const SearchBar = () => {
-    const[manufacturer, setManufacturer]=useState('');
-    const handleSearch = ()=>{
-
-    }
+  const [manufacturer, setManufacturer] = useState("");
+  const handleSearch = () => {};
   return (
     <form className="searchbar" onSubmit={handleSearch}>
-        <div className="searchbar__item">
-            <SearchManufacturer
-            manufacturer={manufacturer}
-            setManufacturer={setManufacturer}
-            />
-        </div>
-
+      <div className="searchbar__item">
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer}
+        />
+      </div>
+      <SearchButton otherClasses="sm:hidden" />
     </form>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
